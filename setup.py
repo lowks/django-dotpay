efbf75edfb6a6f4b926c21fa5f1bd48dd0d8a8e0
Low Kian Seong <kianseong@gmail.com>
2014-12-10 21:28:54 +0800

    Update setup.py
    Adding description to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '0.2.9',
     author = 'Krzysztof Hoffmann',
     author_email = 'krzysiekpl@gmail.com',
+    description = 'Obsługa bramki płatności serwisu dotpay.pl dla Django Używana w serwisie produkcyjnym.',
     license='BSD',
     url = 'https://github.com/hoffmannkrzysztof/django-dotpay/',
     packages = ['dotpay','dotpay.sms','dotpay.payment'],
